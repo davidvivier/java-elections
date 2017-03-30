3caefef08b026d99323b1f0ee9b38847878510ab
kev43 <kev43200@gmail.com>
2017-03-30 15:49:13 +0200

    Remplissage des classes Candidat  (TP1-Q4-5)

--- Elections/src/modele/Candidat.java
+++ Elections/src/modele/Candidat.java
@@ -5,10 +5,79 @@
  */
 package modele;
 
+import java.util.Objects;
+
 /**
  *
  * @author Kevin
  */
-public class Candidat {
-    
+public final class Candidat {
+
+    private CandidatScrutin candidatScrutin;
+    private double pourcentVoix;
+
+    public Candidat(CandidatScrutin candidatScrutin, int nbVotesValides) {
+        this.candidatScrutin = candidatScrutin;
+        this.pourcentVoix = candidatScrutin.getNbVoix() / (double) nbVotesValides;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.candidatScrutin);
+        hash = 37 * hash + (int) (Double.doubleToLongBits(this.pourcentVoix) ^ (Double.doubleToLongBits(this.pourcentVoix) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Candidat other = (Candidat) obj;
+        if (!Objects.equals(this.candidatScrutin, other.candidatScrutin)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.pourcentVoix) != Double.doubleToLongBits(other.pourcentVoix)) {
+            return false;
+        }
+        return true;
+    }
+
+    public double getPourcentVoix() {
+        return pourcentVoix;
+    }
+
+    @Override
+    public String toString() {
+        return "Candidat{" + "candidat=" + candidatScrutin + ", pourcentVoix=" + pourcentVoix + '}';
+    }
+
+    public CandidatScrutin getCandidatScrutin() {
+        return this.candidatScrutin;
+    }
+
+    public int getNbVoix() {
+        return this.candidatScrutin.getNbVoix();
+    }
+
+    public int getDateScrutin() {
+        return this.candidatScrutin.getDateScrutin();
+    }
+
+    public String getNom() {
+        return this.candidatScrutin.getNom();
+    }
+
+    public String getNomParti() {
+        return this.candidatScrutin.getNomParti();
+    }
+
+    public Civilite getCivilite() {
+        return this.candidatScrutin.getCivilite();
+    }
+
 }
