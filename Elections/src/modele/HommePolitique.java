41161ed247be7c745f6eca7ddc8b4465d582799e
kev43 <kev43200@gmail.com>
2017-03-30 14:28:33 +0200

    Remplissage de la classe HommePolitique TP1-Question1

--- Elections/src/modele/HommePolitique.java
+++ Elections/src/modele/HommePolitique.java
@@ -10,5 +10,44 @@ package modele;
  * @author Kevin
  */
 public class HommePolitique {
-    
+
+    private Civilite civilite;
+    private String nom;
+    private String nomParti;
+
+    public HommePolitique(Civilite civilite, String nom, String nomParti) {
+        this.civilite = civilite;
+        this.nom = nom;
+        this.nomParti = nomParti;
+    }
+
+    public Civilite getCivilite() {
+        return civilite;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getNomParti() {
+        return nomParti;
+    }
+
+    public void setCivilite(Civilite civilite) {
+        this.civilite = civilite;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setNomParti(String nomParti) {
+        this.nomParti = nomParti;
+    }
+
+    @Override
+    public String toString() {
+        return "[civilité = " + this.civilite + ", nom = " + this.nom + ", parti = " + this.nomParti + "]";
+    }
+
 }
